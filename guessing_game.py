4c73950a1de4d0ee639f1a64120cc254b9483954
19alex92 <46493443+19alex92@users.noreply.github.com>
2019-01-11 11:39:37 +0100

    Small UI changes

--- guessing_game.py
+++ guessing_game.py
@@ -1,35 +1,7 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 import os
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
 
     random_num = random.randint(1, 10)
     try_count = 0
@@ -46,15 +18,15 @@ def start_game():
 
     def round_guess():
         print("-" * 35)
-        print("Round: {} | You guessed: {} times".format(round_num, try_count))
+        print("Round: {} | Guesses: {}".format(round_num, try_count))
         print("-" * 35)
 
     def hint():
         if try_count > 0:
             if input_user < random_num:
-                print("\nIt's higher than {}, please try again \n".format(input_user))
+                print("\nIt's higher than {}, please try again".format(input_user))
             elif input_user > random_num:
-                print("\nIt's lower than {}, please try again \n".format(input_user))
+                print("\nIt's lower than {}, please try again".format(input_user))
 
     def finish():
         clear_screen()
@@ -73,7 +45,7 @@ def start_game():
 
         try:
             hint()
-            input_user = int(input("What is your guess?\n >   "))
+            input_user = int(input("\nWhat is your guess?\n  >  "))
             if input_user > 10:
                 raise ValueError
         except ValueError as err:
@@ -106,10 +78,5 @@ def start_game():
                 finish()
                 break
 
-
-
-
-
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
