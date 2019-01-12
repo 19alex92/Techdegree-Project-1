e12abc3b705868c638f070a5749795926de0b1b8
19alex92 <46493443+19alex92@users.noreply.github.com>
2019-01-12 08:45:16 +0100

    Moved defs out of def start_game

--- guessing_game.py
+++ guessing_game.py
@@ -1,50 +1,53 @@
 import random
 import os
 
+
+def clear_screen():
+        os.system("cls" if os.name == "nt" else "clear")
+
+def show_score(score):
+    if score < 999:
+        print("-" * 35)
+        print("---------- High Score: {} ----------".format(score))
+
+def round_guess(rounds, tries):
+    print("-" * 35)
+    print("Round: {} | Guesses: {}".format(rounds, tries))
+    print("-" * 35)
+
+def hint(tries, input, random):
+    if tries > 0:
+        if input < random:
+            print("\nIt's higher than {}, please try again".format(input))
+        elif input > random:
+            print("\nIt's lower than {}, please try again".format(input))
+
+def finish(rounds, score):
+    clear_screen()
+    print("#" * 53)
+    print("#### Rounds completed: {} | Your High Score is: {} ####".format(rounds, score))
+    print("#" * 53)
+    print("\n--- Thank you for playing! ---\n")
+
+
 def start_game():
 
     random_num = random.randint(1, 10)
     try_count = 0
     round_num = 1
     high_score = 999
-
-    def clear_screen():
-        os.system("cls" if os.name == "nt" else "clear")
-
-    def show_score():
-        if high_score < 999:
-            print("-" * 35)
-            print("---------- High Score: {} ----------".format(high_score))
-
-    def round_guess():
-        print("-" * 35)
-        print("Round: {} | Guesses: {}".format(round_num, try_count))
-        print("-" * 35)
-
-    def hint():
-        if try_count > 0:
-            if input_user < random_num:
-                print("\nIt's higher than {}, please try again".format(input_user))
-            elif input_user > random_num:
-                print("\nIt's lower than {}, please try again".format(input_user))
-
-    def finish():
-        clear_screen()
-        print("#" * 53)
-        print("#### Rounds completed: {} | Your High Score is: {} ####".format(round_num, high_score))
-        print("#" * 53)
-        print("\n--- Thank you for playing! ---\n")
+    input_user = None
        
     clear_screen()
     print("--- Welcome to the number guessing game ---\n")
     print("Please enter a number between 1 and 10, good luck!\n")
 
     while True:
-        show_score()
-        round_guess()
+        show_score(high_score)
+        round_guess(round_num, try_count)
 
         try:
-            hint()
+            hint(try_count, input_user, random_num)
             input_user = int(input("\nWhat is your guess?\n  >  "))
             if input_user > 10 or input_user < 1:
                 raise ValueError
@@ -75,7 +78,7 @@ def start_game():
                 clear_screen()
                 continue
             else:
-                finish()
+                finish(round_num, high_score)
                 break
 
 if __name__ == '__main__':
